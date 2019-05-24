84a522e99eb17c6b42d48133393fd7f636fa59a4
edel <edelmira.oriflame.gt@gmail.com>
2019-05-24 01:45:21 -0600

    manuela

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+ $destino="edelmira.oriflame.gt@gmail.com";
+ $nombre=$_POST["name"];
+ $correo=$_POST["email"];;
+ $mensaje=$_POST["message"];
+ $contenido="Nombre: ".$nombre."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
+ mail($destino,"Contacto", $contenido);
+ header("Location: index.html");
+
+?>
